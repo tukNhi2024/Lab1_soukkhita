9d5b923469a5ef7effb0ff5dad95c800197b6d8e
tukNhi2024 <166972149+tukNhi2024@users.noreply.github.com>
2024-04-14 14:13:07 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Hello from './src/Hello';
+import CapTaps from './src/CapTaps';
+import CustomCom from './src/CustomCom'
+import Stateprops from './src/Stateprops'
+import Styling from './src/Styling'
+import ScrollableCont from './src/ScrollableCont'
+import BuildingForm from './src/BuildingForm'
+import LongList from './src/LongList'
+
+
+export default function App() {
+  return (
+      <Hello/>
+  );
+}
+
+
